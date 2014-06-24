ecbdac732cb3c84fb91d8b04744435919abca022
Gian-Marco Baschera <gba@zuehlke.com>
2014-06-24 17:11:35 +0200

    rename file and automatically register cluster for parallel processing

--- src/loadRequiredLibraries.R
+++ src/loadRequiredLibraries.R
@@ -1,7 +1,11 @@
-loadRequiredLibraries <- function() {
+setupWorkEnvironment <- function() {
   library('R.matlab')
   library('fastICA')
   library('foreach')
   library('doSNOW')
   library('glmnet')
+  
+  NUMBER_OF_CORES = 4
+  cl <- makeCluster(NUMBER_OF_CORES)
+  registerDoSNOW(cl)
 }
