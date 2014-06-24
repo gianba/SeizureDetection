9a0964394ffad8b9aad75d774f1e2c6a9361fba6
Gian-Marco Baschera <gba@zuehlke.com>
2014-06-24 18:26:20 +0200

    renaming bug fixed

--- src/features/extractFeatures.R
+++ src/features/extractFeatures.R
@@ -1,7 +1,7 @@
 # Extracts the features from the eegData
 extractFeatures <- function(eegData, icaWeights) {
   # calculate ica components
-  icaData <- t(t(erpData-rowMeans(eegData)) %*% icaWeights)
+  icaData <- t(t(eegData-rowMeans(eegData)) %*% icaWeights)
 
   # Every getXXXFeatures function has to return a data.frame with one row and several columns as features.
   # The columns should be named after the extracted feature.
