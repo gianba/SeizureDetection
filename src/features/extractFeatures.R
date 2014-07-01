f4946d07e7b657be862a8d4d5a74d2ce7924862b
Fran <franciscoblayagonzalvez@digital.cabinet-office.gov.uk>
2014-07-01 21:43:54 +0100

    Prepare eeg filtering test
    - Use only frequency features for the test

--- src/features/extractFeatures.R
+++ src/features/extractFeatures.R
@@ -1,22 +1,22 @@
-# Extracts the features from the eegData
-extractFeatures <- function(eegData, icaWeights, pcaWeights) {
-  # calculate ica components
+# Extracts the features from the eegClipData
+extractFeatures <- function(eegClipData, icaWeights, pcaWeights) {
   NOF_COMPONENTS <- 5
-  icaData <- t(t(eegData-rowMeans(eegData)) %*% icaWeights[,1:NOF_COMPONENTS])
-  pcaData <- t(t(eegData) %*% pcaWeights[,1:NOF_COMPONENTS])
+  # Project data into first NOF_COMPONENTS components for ICA and PCA. Components are arranged in rows
+  icaData <- t(t(eegClipData-rowMeans(eegClipData)) %*% icaWeights[,1:NOF_COMPONENTS])
+  pcaData <- t(t(eegClipData) %*% pcaWeights[,1:NOF_COMPONENTS])
 
   # Every getXXXFeatures function has to return a data.frame with one row and several columns as features.
   # The columns should be named after the extracted feature.
   # The features can be appended to the returning "features" data.frame using cbind()
   # features <- cbind(getXXXFeatures(...), features)
-  features <- getFrequencyFeatures(eegData,'SpecRaw')
+  features <- getFrequencyFeatures(eegClipData,'SpecRaw')
   features <- cbind(getFrequencyFeatures(icaData,'SpecICA',NOF_COMPONENTS), features)
   features <- cbind(getFrequencyFeatures(pcaData,'SpecPCA',NOF_COMPONENTS), features)
-  features <- cbind(getFractalDimFeatures(eegData[c(1,5,9,13),],'Raw'), features)
-  features <- cbind(getFractalDimFeatures(icaData,'ICA'), features)
-  features <- cbind(getFractalDimFeatures(pcaData,'PCA'), features)
-  features <- cbind(getLyapunovFeature(icaData,'ICA'),features)
-  features <- cbind(getLyapunovFeature(pcaData[1:4,],'PCA'),features)
+  #features <- cbind(getFractalDimFeatures(eegClipData[c(1,5,9,13),],'Raw'), features)
+  #features <- cbind(getFractalDimFeatures(icaData,'ICA'), features)
+  #features <- cbind(getFractalDimFeatures(pcaData,'PCA'), features)
+  #features <- cbind(getLyapunovFeature(icaData,'ICA'),features)
+  #features <- cbind(getLyapunovFeature(pcaData[1:4,],'PCA'),features)
   
   return(features)
 }
