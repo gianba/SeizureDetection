c930d499e0c0e7980278b1ced141d7b041bc58df
scheuchzer <thomas.scheuchzer@gmx.net>
2014-07-08 07:44:04 +0200

    #13 moved code into methods

--- src/doredis/example/demo.R
+++ src/doredis/example/demo.R
@@ -1,19 +1,23 @@
-host =  "redis.java-adventures.com"
-port = 6379
-queue = "test"
-password = "kaggle1+"
-loadRedis <- function() {
-  require('doRedis')
+
+initRedis <- function(password) {
+  host =  "redis.java-adventures.com"
+  port = 6379
+  queue = "test"
+  if(is.na(password)) stop("Please provide redis password")
+  
+  library('doRedis')
   registerDoRedis(host =  host, port = port, queue = queue, password = password )
 }
 resetRedis <- function() {
   removeQueue(queue)
 }
-startWorker <- function() {
+startWorkerAndWait <- function(password) {
+  if(is.na(password)) stop("Please provide redis password")
   require('doRedis')
   redisWorker(host =  host, port = port, queue = queue, password = password )
 }
-startLocalWorkert <- function(workerCount) {
+startLocalWorker <- function(workerCount, password) {
+  if(is.na(password)) stop("Please provide redis password")
   require('doRedis')
   startLocalWorkers(n = workerCound, host =  host, port = port, queue = queue, password = password )
 }
--- src/doredis/example/worker.R
+++ src/doredis/example/worker.R
@@ -1,12 +1,14 @@
 #!/usr/bin/Rscript
-host =  "redis.java-adventures.com"
-port = 6379
-queue = "test"
-password = "kaggle1+"
 
-startWorker <- function() {
+main <- function(password) {
+  host =  "redis.java-adventures.com"
+  port = 6379
+  queue = "test"
+  if(is.na(password)) stop("Please provide redis password")
+
   library('doRedis')
   redisWorker(host =  host, port = port, queue = queue, password = password )
 }
 
-startWorker()
+password=commandArgs(TRUE)[1]
+main(password = password)
