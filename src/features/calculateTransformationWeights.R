5c89f178e67a9d1d6ee2b3a48f9c060f12b9e860
unknown <fbg@LONN0189.ads.zuehlke.com>
2014-07-09 08:20:26 +0100

    Parallelise clip filtering and assembling

--- src/features/calculateTransformationWeights.R
+++ src/features/calculateTransformationWeights.R
@@ -1,23 +1,23 @@
 calculateTransformationWeights <- function(folderPath) {
-  signal <- NULL
-  files <- list.files(folderPath)
-  for(file in files) {
-    if(!grepl('_test_', file)) {
-      filePath <- getPath(folderPath, file)
-      mat <- readMat(filePath)
-      nofSamples <- ncol(mat$data)
-      if(nofSamples>1000) {
-        everyNth <- floor(nofSamples/1000)
-        mat$data <- mat$data[,seq(1,nofSamples,everyNth)]
-      }
-      if(is.null(signal)){
-        signal = mat$data
-      } else {
-        signal = cbind(signal, mat$data)
-      }
+  files <- list.files(folderPath, pattern='*ictal*')
+  filteredClips <- hash()
+  logMsg('Filter and join clips')
+  signal <- foreach(i=1:length(files),.combine='cbind',.packages=usedPackages,.export=userFunctions) %dopar% {
+    file <- files[i]
+    filePath <- getPath(folderPath, file)
+    mat <- readMat(filePath)
+    nofSamples <- ncol(mat$data)
+    if(nofSamples>1000) {
+      everyNth <- floor(nofSamples/1000)
+      mat$data <- mat$data[,seq(1,nofSamples,everyNth)]
     }
+    filteredClip <- getFilteredSignal(mat$data)
+    filteredClips$file = filteredClip
+    filteredClip
   }
+  logMsg('Calculate ICA weights')
   a <- fastICA(t(signal), n.comp=16, alg.typ='parallel')
+  logMsg('Calculate PCA weights')
   p <- princomp(t(signal))
-  return(list(ica=a$K %*% a$W, pca=p$loadings, original=signal))
+  return(list(ica=a$K %*% a$W, pca=p$loadings, original=signal, filteredClips=filteredClips))
 }
