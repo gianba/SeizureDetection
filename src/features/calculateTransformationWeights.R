d5a47ba27fb3a62a4037db50d5e78cac5bd3deef
Fran <fcoblaya@gmail.com>
2014-07-09 19:30:23 +0100

    Simplify foreach

--- src/features/calculateTransformationWeights.R
+++ src/features/calculateTransformationWeights.R
@@ -1,9 +1,7 @@
 calculateTransformationWeights <- function(folderPath) {
   files <- list.files(folderPath, pattern='*ictal*')
-  signal <- foreach(i=1:length(files),.combine='cbind',.export=userFunctions) %dopar% {
-    file <- files[i]
-    filePath <- getPath(folderPath, file)
-    mat <- readMat(filePath)
+  signal <- foreach(i=1:length(files),.combine='cbind',.packages=usedPackages,.export=userFunctions) %dopar% {
+    mat <- readMat(getPath(folderPath, files[i]))
     nofSamples <- ncol(mat$data)
     if(nofSamples>1000) {
       everyNth <- floor(nofSamples/1000)
