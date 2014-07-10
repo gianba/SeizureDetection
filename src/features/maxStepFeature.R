241c41b89a5cf6ef589eb3bdb1ee38587e1f80d5
Gian-Marco Baschera <gba@zuehlke.com>
2014-07-10 12:59:00 +0200

    For now we just compute max step over the entire clip. no early and late...

--- src/features/maxStepFeature.R
+++ src/features/maxStepFeature.R
@@ -4,8 +4,8 @@ getMaxStepFeatures <- function(eegData, name) {
   nChannel <- dim(eegData)[1]
   nSamples <- dim(eegData)[2]
   
-  features <- matrix(nrow=1, ncol=nofTimeIntervals*3)
-  names <- vector(mode='character', length=nofTimeIntervals*3)
+  features <- matrix(nrow=1, ncol=nofTimeIntervals)
+  names <- vector(mode='character', length=nofTimeIntervals)
   
   for(i in 1:nofTimeIntervals) {
     dataShift <- round(nSamples*time_intervals_ms[i]/1000)
@@ -13,12 +13,12 @@ getMaxStepFeatures <- function(eegData, name) {
     shiftData <- cbind(matrix(0,nrow=nChannel,ncol=dataShift),eegData)
     steps <- shiftData - noShiftData
     validSteps <- abs(steps[,(1+dataShift):(nSamples-dataShift)])
-    features[1,i*3-2] <- max(validSteps)
-    names[i*3-2] <- paste('MaxStep',name,time_intervals_ms[i],'ms',sep="")
-    features[1,i*3-1] <- max(validSteps[,1:round((nSamples-2*dataShift)/3)])
-    names[i*3-1] <- paste('MaxStepEarly',name,time_intervals_ms[i],'ms',sep="")
-    features[1,i*3] <- max(validSteps[,round(2*(nSamples-2*dataShift)/3):(nSamples-2*dataShift)])
-    names[i*3] <- paste('MaxStepLate',name,time_intervals_ms[i],'ms',sep="")
+    features[1,i] <- max(validSteps)
+    names[i] <- paste('MaxStep',name,time_intervals_ms[i],'ms',sep="")
+#    features[1,i*3-1] <- max(validSteps[,1:round((nSamples-2*dataShift)/3)])
+#    names[i*3-1] <- paste('MaxStepEarly',name,time_intervals_ms[i],'ms',sep="")
+#    features[1,i*3] <- max(validSteps[,round(2*(nSamples-2*dataShift)/3):(nSamples-2*dataShift)])
+#    names[i*3] <- paste('MaxStepLate',name,time_intervals_ms[i],'ms',sep="")
   }
   
   colnames(features) <- names
